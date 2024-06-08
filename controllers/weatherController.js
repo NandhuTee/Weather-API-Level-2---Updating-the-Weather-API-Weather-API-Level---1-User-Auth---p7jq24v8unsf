dd9801f120cda726198377c022673ed49463bb37
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2024-06-08 08:40:31 +0530

    Update weatherController.js

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -5,8 +5,12 @@ const getWeatherByCityName = async (req, res) => {
 
   try {
     // TODO: Implement logic to retrieve weather data by city name
+    const weatherData = await WeatherData.findOne({cityName})
+    if(!weatherData){
+      return res.status(404).json({ message: 'Weather data not found for the given city' });
+    }
     // Example response when data is found:
-    // res.status(200).json({ weatherData });
+    return res.status(200).json({ weatherData });
     // Example response when data is not found:
     // res.status(404).json({ message: 'Weather data not found for the given city' });
   } catch (error) {
@@ -21,6 +25,12 @@ const getWeatherByZipCode = async (req, res) => {
 
   try {
     // TODO: Implement logic to retrieve weather data by zip code
+    const weatherData = await WeatherData.findOne({zipCode})
+    if(!weatherData){
+      return res.status(404).json({ message: 'Weather data not found for the given zip code' });
+    }
+    // Example response when data is found:
+    return res.status(200).json({ weatherData });
     // Example response when data is found:
     // res.status(200).json({ weatherData });
     // Example response when data is not found:
@@ -35,18 +45,16 @@ const getWeatherByZipCode = async (req, res) => {
 const postWeatherAlert = async (req, res) => {
   const { cityName, humidity, weatherDescription, temperature, zipCode } =
     req.body;
-
   try {
-    // TODO: Implement logic to post weather alert
     // Example response when alert is posted successfully:
-    // res.status(201).json({ message: 'Weather alert posted successfully', alert: newAlert });
+    const newAlert = await WeatherData.create({cityName, humidity, weatherDescription, temperature, zipCode})
+    return res.status(201).json({ message: 'Weather alert posted successfully', alert: newAlert });
   } catch (error) {
     res
       .status(500)
       .json({ message: 'Internal server error', error: error.message });
   }
 };
-
 module.exports = {
   getWeatherByCityName,
   getWeatherByZipCode,
